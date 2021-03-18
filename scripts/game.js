d70f9681914ccbedf280e9c1dd27bb0cbc09a4f1
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-03-18 16:54:10 -0300

    game over almost done

--- scripts/game.js
+++ scripts/game.js
@@ -23,51 +23,57 @@ function handleMove(position) {
   // you cannot put same symbol on same square
   if (board[position] == "") {
     board[position] = symbols[playerTime];
-    
-    
+
     //verifying if has a winner
-    gameOver = isWin()
+    gameOver = isWin();
 
-    //changing the player time
-
-    if (playerTime == 0) {
-      playerTime = 1;
-    } else {
-      playerTime = 0;
+    // the game only continues if game over is  false
+    if (!gameOver) {
+      //changing the player time
+      if (playerTime == 0) {
+        playerTime = 1;
+      } else {
+        playerTime = 0;
+      }
     }
   }
+
+  return gameOver;
 }
 
+function isWin() {
+  let winStates = [
+    //this sub arrays presents the position on the board that
+    //refers to a win on the game rules
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6],
+  ];
 
-function isWin(){
-    let winStates = [
-        //this sub arrays presents the position on the board that
-        //refers to a win on the game rules
-        [0,1,2],
-        [3,4,5],
-        [6,7,8],
-        [0,3,6],
-        [1,4,7],
-        [2,5,8],
-        [0,4,8],
-        [2,4,6]
-    ] 
+  for (let i = 0; i < winStates.length; i++) {
+    //reading the wins alternatives
+    let sequence = winStates[i];
 
-    for(let i = 0; i < winStates.length; i++){
-        //reading the wins alternatives
-        let sequence = winStates[i]
+    //naming positions
+    let pos1 = sequence[0];
+    let pos2 = sequence[1];
+    let pos3 = sequence[2];
 
-        //naming positions
-        let pos1 = sequence[0]
-        let pos2 = sequence[1]
-        let pos3 = sequence[2]
-
-        //declaring the winner
-        if(board[pos1] == board[pos2] && 
-            board[pos1] == board[pos3]){
-                return true
-            }
+    //declaring the winner
+    if (
+      board[pos1] == board[pos2] &&
+      board[pos1] == board[pos3] &&
+      //the blank squares cannot means a win too
+      board[pos1] != ""
+    ) {
+      return true;
     }
+  }
 
-    return false
+  return false;
 }
