239ee6a1939e0bc2c4db9d9a65c585099236ff08
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-03-18 11:19:07 -0300

    scripts created, start to declare vars

---
+++ scripts/game.js
@@ -0,0 +1,9 @@
+//THIS FILE IS RESPONSABLE TO CONTROL THE GAME FUNCTIONS
+// AND SERVICES RULES
+
+//NAMING VARIABLES
+let board = ['', '', '', '', '', '', '', '', ''];
+
+let playerTime = 0
+
+let symbols = ['o', 'x']
