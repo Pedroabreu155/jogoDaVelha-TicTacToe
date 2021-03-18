cccb6c05a8c29260eaf3ff79d089d3c0f25d4efc
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-03-18 12:07:36 -0300

    starting the gameOver function

--- scripts/game.js
+++ scripts/game.js
@@ -8,19 +8,66 @@ let playerTime = 0;
 
 let symbols = ["o", "x"];
 
+let gameOver = false;
+
 //game functions
 
 function handleMove(position) {
-    // this if is to control the squares
-    // you cannot put same symbol on same square
-  if(board[position] == '' ){
-  board[position] = symbols[playerTime];
+  //making the game over
 
-  if (playerTime == 0) {
-    playerTime = 1;
-  } else {
-    playerTime = 0;
+  if (gameOver) {
+    return;
   }
 
+  // this if is to control the squares
+  // you cannot put same symbol on same square
+  if (board[position] == "") {
+    board[position] = symbols[playerTime];
+    
+    
+    //verifying if has a winner
+    gameOver = isWin()
+
+    //changing the player time
+
+    if (playerTime == 0) {
+      playerTime = 1;
+    } else {
+      playerTime = 0;
+    }
+  }
 }
+
+
+function isWin(){
+    let winStates = [
+        //this sub arrays presents the position on the board that
+        //refers to a win on the game rules
+        [0,1,2],
+        [3,4,5],
+        [6,7,8],
+        [0,3,6],
+        [1,4,7],
+        [2,5,8],
+        [0,4,8],
+        [2,4,6]
+    ] 
+
+    for(let i = 0; i < winStates.length; i++){
+        //reading the wins alternatives
+        let sequence = winStates[i]
+
+        //naming positions
+        let pos1 = sequence[0]
+        let pos2 = sequence[1]
+        let pos3 = sequence[2]
+
+        //declaring the winner
+        if(board[pos1] == board[pos2] && 
+            board[pos1] == board[pos3]){
+                return true
+            }
+    }
+
+    return false
 }
