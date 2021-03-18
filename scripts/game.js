4546657d45f93294bc62219db761072415f9ac0c
Pedro Abreu <pedroliveirabreu@gmail.com>
2021-03-18 11:37:56 -0300

    putting symbols done

--- scripts/game.js
+++ scripts/game.js
@@ -2,8 +2,20 @@
 // AND SERVICES RULES
 
 //NAMING VARIABLES
-let board = ['', '', '', '', '', '', '', '', ''];
+let board = ["", "", "", "", "", "", "", "", ""];
 
-let playerTime = 0
+let playerTime = 0;
 
-let symbols = ['o', 'x']
+let symbols = ["o", "x"];
+
+//game functions
+
+function handleMove(position) {
+  board[position] = symbols[playerTime];
+
+  if (playerTime == 0) {
+    playerTime = 1;
+  } else {
+    playerTime = 0;
+  }
+}
--- scripts/interface.js
+++ scripts/interface.js
@@ -1,2 +1,37 @@
 //THIS FILE IS RESPONSABLE TO CONTROL THE INTERFACE
 // AND THE CHANGES ON HTML
+
+document.addEventListener("DOMContentLoaded", () => {
+  let squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+    square.addEventListener("click", handleClick);
+  });
+});
+
+function handleClick(event) {
+  // test log
+  // console.log(event.target)
+
+  let square = event.target;
+  let position = square.id;
+
+  handleMove(position);
+
+  updateSquares();
+}
+
+function updateSquares() {
+    let squares = document.querySelectorAll(".square");
+
+  squares.forEach((square) => {
+        let position = square.id
+        let symbol = board[position]
+
+        if(symbol != ''){
+            square.innerHTML = `<div class= '${symbol}'></div>`
+        }
+
+
+    });
+}
